ef2bb7bfaa7112e61b3b914933a14c5f56942d6d
firshta2016 <firshtashefa@gmail.com>
2021-02-13 21:25:31 -0800

    first commit

---
+++ Angry Professor.js
@@ -0,0 +1,11 @@
+/**A Discrete Mathematics professor has a class of students. Frustrated with their lack of discipline, 
+the professor decides to cancel class if fewer than some number of students are present when class starts. 
+Arrival times go from on time (arivalTime <= 0) to arrived late (arrivalTime > 0).
+Given the arrival time of each student and a threshhold number of attendees, determine if the class is cancelled. */
+
+function angryProfessor(k, a) {
+    
+ }
+
+ const result = angryProfessor(3, [-1, -3, 4, 2]);
+ console.log(result);
---
+++ Beautiful Days At The Movies.js
@@ -0,0 +1,10 @@
+/** Given a range of numbered days, [i...j] and a number k, determine the number of days in the range that are beautiful. 
+Beautiful numbers are defined as numbers where [i - reverse(i)] is evenly divisible by k. 
+If a day's value is a beautiful number, it is a beautiful day. Return the number of beautiful days in the range. */
+
+function beautifulDays(i, j, k) {
+   
+}
+
+const result = beautifulDays(20, 23, 6)
+console.log(result); //2
