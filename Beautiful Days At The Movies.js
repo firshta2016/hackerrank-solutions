d0d0167f0b9532a80130f8e9c1c66b32af49a8c8
firshta2016 <firshtashefa@gmail.com>
2021-02-14 21:38:31 -0800

    beautifuldaysol

--- Beautiful Days At The Movies.js
+++ Beautiful Days At The Movies.js
@@ -1,10 +1,57 @@
-/** Given a range of numbered days, [i...j] and a number k, determine the number of days in the range that are beautiful. 
-Beautiful numbers are defined as numbers where [i - reverse(i)] is evenly divisible by k. 
-If a day's value is a beautiful number, it is a beautiful day. Return the number of beautiful days in the range. */
+'use strict';
 
-function beautifulDays(i, j, k) {
-   
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
 }
 
-const result = beautifulDays(20, 23, 6)
-console.log(result); //2
+// Complete the beautifulDays function below.
+function beautifulDays(i, j, k) {
+    let count = 0;
+  for(let x = i; x <= j; x++) {
+    let reverseArr = x.toString().split('').reverse()
+    .join('');
+    let solution = Math.abs((x - reverseArr) / k) ;
+    if (Number.isInteger(solution)) {
+      count++
+    }
+  }
+  return count;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const ijk = readLine().split(' ');
+
+    const i = parseInt(ijk[0], 10);
+
+    const j = parseInt(ijk[1], 10);
+
+    const k = parseInt(ijk[2], 10);
+
+    let result = beautifulDays(i, j, k);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
