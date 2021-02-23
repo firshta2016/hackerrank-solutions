7c668ca936222d8e9c349f132e2833ad00a1ca68
firshta2016 <firshtashefa@gmail.com>
2021-02-23 08:44:17 -0800

    added angryprof

--- Angry Professor.js
+++ Angry Professor.js
@@ -1,11 +1,63 @@
-/**A Discrete Mathematics professor has a class of students. Frustrated with their lack of discipline, 
-the professor decides to cancel class if fewer than some number of students are present when class starts. 
-Arrival times go from on time (arivalTime <= 0) to arrived late (arrivalTime > 0).
-Given the arrival time of each student and a threshhold number of attendees, determine if the class is cancelled. */
+'use strict';
 
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the angryProfessor function below.
 function angryProfessor(k, a) {
-    
- }
+     let count = 0;
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] <= 0) {
+            count++
+        } 
+    }
+    //return count >= k ? 'NO' : 'YES'
+    if (count >= k) {
+        return 'NO'
+    } else {
+        return 'YES'
+    }
+}
 
- const result = angryProfessor(3, [-1, -3, 4, 2]);
- console.log(result);
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const t = parseInt(readLine(), 10);
+
+    for (let tItr = 0; tItr < t; tItr++) {
+        const nk = readLine().split(' ');
+
+        const n = parseInt(nk[0], 10);
+
+        const k = parseInt(nk[1], 10);
+
+        const a = readLine().split(' ').map(aTemp => parseInt(aTemp, 10));
+
+        let result = angryProfessor(k, a);
+
+        ws.write(result + "\n");
+    }
+
+    ws.end();
+}
