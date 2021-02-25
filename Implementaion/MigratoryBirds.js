e8feb310a99ba14e144832d78754384378bc7388
firshta2016 <firshtashefa@gmail.com>
2021-02-25 13:32:49 -0800

    added new challenges

---
+++ Implementaion/MigratoryBirds.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the migratoryBirds function below.
+function migratoryBirds(arr) {
+    let map = {}, maxCount = 0, output = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (!map[arr[i]]) {
+            map[arr[i]] = 1;
+        } else {
+            map[arr[i]] += 1;
+        }
+
+        // getting max count
+        if (map[arr[i]] > maxCount) {
+            maxCount = map[arr[i]];
+        }
+    }
+
+    for (let j = 0; j < arr.length; j++) {
+        if (map[arr[j]] === maxCount) {
+            output.push(arr[j]);
+        }
+    }
+    return Math.min(...output);
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const arrCount = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = migratoryBirds(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
