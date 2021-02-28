b19cb71de74ac268c95f428462003b4781b62780
firshta2016 <firshtashefa@gmail.com>
2021-02-27 21:51:00 -0800

    added challenge

---
+++ Warmup/MiniMaxSum.js
@@ -0,0 +1,43 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the miniMaxSum function below.
+function miniMaxSum(arr) {
+    let min = Math.min(...arr);
+    let max = Math.max(...arr);
+    let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  } 
+  let maxNum = sum - min;
+  let minNum = sum - max;
+  console.log(minNum, maxNum)
+}
+
+function main() {
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    miniMaxSum(arr);
+}
